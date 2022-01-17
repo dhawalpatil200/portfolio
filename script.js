9d2f2cfd264dda8b0aa63471b815b57621513806
dhawalpatil200 <71254556+dhawalpatil200@users.noreply.github.com>
2022-01-17 19:33:51 +0530

    First Commit

---
+++ script.js
@@ -0,0 +1,66 @@
+const particles = [];
+
+//p5.js
+function setup() {
+  const canvas = createCanvas(window.innerWidth, window.innerHeight);
+  canvas.parent("canvasForHTML");
+  const particlesLength = Math.floor(window.innerWidth / 13);
+
+  for (let i = 0; i < particlesLength; i++) {
+    particles.push(new Particle());
+  }
+}
+
+function draw() {
+  background("rgb(57, 70, 121)");
+  clear();
+  particles.forEach((p, index) => {
+    p.update();
+    p.draw();
+    p.checkParticles(particles.slice(index));
+  });
+}
+
+class Particle {
+  constructor() {
+    this.size = 10;
+    this.pos = createVector(random(width), random(height));
+    this.vel = createVector(random(-1.2, 1.2), random(-1.2, 1.2));
+  }
+
+  // draw particle
+  draw() {
+    noStroke();
+    fill("rgba(255,255,255,0.2)");
+    circle(this.pos.x, this.pos.y, this.size);
+  }
+
+  //update movement
+  update() {
+    this.pos.x += this.vel.x;
+    this.pos.y += this.vel.y;
+    this.edges();
+  }
+
+  // Detect edges
+  edges() {
+    if (this.pos.x < 0 || this.pos.x > width) {
+      this.vel.x *= -1;
+    }
+
+    if (this.pos.y < 0 || this.pos.y > height) {
+      this.vel.y *= -1;
+    }
+  }
+
+  //connect particles
+  checkParticles(particles) {
+    particles.forEach((particle) => {
+      const d = dist(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+      if (d < 120) {
+        stroke("rgba(255,255,255,0.1)");
+        line(this.pos.x, this.pos.y, particle.pos.x, particle.pos.y);
+      }
+    });
+  }
+}
